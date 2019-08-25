c32c46f41b41ff1e0cf89db24ca7a073789a2433
Alex Gurin <dn070287gav@gmail.com>
2019-08-25 10:01:20 +0300

    Initial Commit

---
+++ UICollectionViewCompositionalLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionViewCompositionalLayout
+//
+//  Created by Alex Gurin on 8/25/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
